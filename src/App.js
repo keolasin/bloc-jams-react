1727f38d42a9aa97fe9ea9043f2a02a6cd5b3d07
Keolasin <43076895+keolasin@users.noreply.github.com>
2019-01-30 20:51:21 -0800

    assignment final

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {Router, Link} from 'react-router-dom';
+import {Route, Link} from 'react-router-dom';
 import './App.css';
 import Landing from './components/Landing.js';
 import Library from './components/Library.js';
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BroweserRouter} from 'react-router-dom';
+import {BrowserRouter} from 'react-router-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
@@ -8,7 +8,7 @@ import * as serviceWorker from './serviceWorker';
 ReactDOM.render(
   <BrowserRouter>
     <App />
-  <BrowserRouter />, document.getElementById('root')
+  </BrowserRouter>, document.getElementById('root')
 );
 
 
