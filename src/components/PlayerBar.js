283b8bb1e924aac7ee20fc6e962ed1c9e463d4a6
Matthew Reyes <43076895+keolasin@users.noreply.github.com>
2019-02-05 08:52:12 -0800

    buttons add to PlayerBar

--- src/components/PlayerBar.js
+++ src/components/PlayerBar.js
@@ -4,7 +4,34 @@ class PlayerBar extends Component {
   render(){
     return(
       <section className="player-bar">
-        Player bar will go here
+        <section id="buttons">
+      
+          <button id="previous">
+            <span className="icon ion-md-rewind"></span>
+          </button>
+          
+          <button id="play-pause">
+            <span className="icon ion-md-play"></span>
+            <span className="icon ion-md-pause"></span>
+          </button>
+      
+          <button id="next">
+            <span className="icon ion-md-fastforward"></span>
+          </button>
+      
+          <button id="time-control">
+            <div className="current-time">-:--</div>
+            <input type="range" className="seek-bar" value="0" />
+            <div className="total-time">-:--</div>
+          </button>
+      
+          <button id="volume-control">
+            <div className="icon ion-volume-low"></div>
+            <input type="range" className="seek-bar" value="80" />
+            <div className="icon ion-volume-high"></div>
+      
+          </button>
+        </section>
       </section>
     );
   }
