9eb098b862ddc4f3d7c1d0a6f3b4dfa8e37a54cb
Matthew Reyes <43076895+keolasin@users.noreply.github.com>
2019-02-05 08:40:00 -0800

    new PlayerBar component

---
+++ src/components/PlayerBar.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class PlayerBar extends Component {
+  render(){
+    return(
+      <section className="player-bar">
+        Player bar will go here
+      </section>
+    );
+  }
+}
+
+export default PlayerBar;
